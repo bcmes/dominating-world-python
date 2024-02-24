f20b629559c9646a4feb89115a6d307353613598
bcmes <brunomeloesilva@gmail.com>
2024-02-24 09:01:09 -0400

    initial commit

---
+++ seePrint.py
@@ -0,0 +1,12 @@
+# simples
+print('Hello Biruleibo.')
+
+# com varios valores
+print('Hello', 'Biruleibo.')
+
+# alterando o separador padrao
+print('Hello', 'Biruleibo.', sep='-')
+
+# alterando o caractere de quebra de linha. Por padrão no Win é \r\n em unix é \n
+print('Hello', 'Biruleibo.', sep='-', end='#')
+print('Hello', 'Biruleibo.', sep='-', end='#')
